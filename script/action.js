b3b9ebd866c506adc498d7077a339daa5c69f8f7
Leehyeji16 <tkdqhr3011@gmail.com>
2026-03-16 23:47:37 +0900

    Add files via upload

--- script/action.js
+++ script/action.js
@@ -4,23 +4,23 @@ $('.ico_search').click(function(){
 
     if(k == 0){
         $('.search_box').fadeIn(100, function(){
-        $('.search_box').animate({top:'50%'},700);
-    })
+            $('.search_box').animate({top:'50%'}, 700);
+        })
         k = 1;
     }
     else{
-    k = 0
-    $('.search_box').fadeOut(300, function(){
-        $('.search_box').css({top:''})
-    })
-}
+        k = 0
+        $('.search_box').fadeOut(300, function(){
+            $('.search_box').css({top:'-500px'})
+        })
+    }
     console.log("k=",k)
 })
 
 $('.btn_close').click(function(){
     $('.visual').removeClass('on');
     $('.search_box').fadeOut(300, function(){
-        $('.search_box').css({top:''})
+        $('.search_box').css({top:'-500px'})
     })
     k = 0;
     return false
@@ -28,19 +28,25 @@ $('.btn_close').click(function(){
 
 
 
-
-
 $('.mo_nav').html($('.pc_nav').html())
-$('.mo_icons').html($('.icons').html())
+$('.mo_icons').html($('.icons a img').map(function(){
+    return '<a href="#">' + $(this).prop('outerHTML') + '</a>';
+}).get().join(''))
 
-//mo nav
 $('.hamburger').click(function(){
-    $('.mo_wrap').show()
+    $('.mo_wrap').css('display', 'flex').addClass('open');
 })
 
 $('.mo_wrap').click(function(){
-    $(this).hide();
+    $(this).hide().removeClass('open');
 })
+
+$('.mo_close').click(function(e){
+    e.stopPropagation();
+    $('.mo_wrap').hide().removeClass('open');
+})
+
 $('.mo_nav').click(function(e){
-    e.stopPropagation();   //이벤트 전파 차단
+    e.stopPropagation();
 })
+
